3b54e51b55346aab1921867049f66141631de63a
AliRn <alirn76@yahoo.com>
2022-02-23 18:27:18 +0330

    Change Url Syntax

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,9 @@
+from app.apis import logout
 
-
-
-
+# ClassBase or function_base (if function_base, how should we handle decorators, seperate or all together?)
 urls = {
     '': Class2.api(),
-    'logout/': Logout.api(),
+    'logout/': logout,
     'user/': {
         '<int>/': SingleUser.api(),
         'lists/': UsersList.api(),
