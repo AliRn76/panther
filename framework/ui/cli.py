59bfd1d3ddeb7dee36aae28adfa863eda7bb59e0
MahanBi <dirtymbgamer@gmail.com>
2022-02-24 07:15:29 +0330

    Add metaclass Singleton

---
+++ framework/ui/cli.py
@@ -0,0 +1,17 @@
+from argparse import ArgumentParser
+from ..metaclasses import Singleton
+
+
+class CliParser(metaclass=Singleton):
+    def __init__(self) -> None:
+        self.parser = ArgumentParser()
+
+    @property
+    def parser(self) -> ArgumentParser:
+        self.__parser.add_argument('', '', help='')
+
+        return self.__parser
+
+    @parser.setter
+    def parser(self, new_parser):
+        self.__parser = new_parser
