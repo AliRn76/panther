2057b1e1e0e7f37801a08b749d782d262cca3a9e
AliRn76 <alirn76@yahoo.com>
2022-03-23 03:20:32 +0430

    Test most types as data in Response + Upgrade in PyPI

--- example/app/apis.py
+++ example/app/apis.py
@@ -6,18 +6,49 @@ from panther.request import Request
 from panther.app import API
 
 
-@API.post(input_model=UserInputSerializer, output_model=UserOutputSerializer)
+@API.post(input_model=UserInputSerializer)
 async def single_user(request: Request):
     # print(f'{request.data = }')
-    user = User.create(username='ali', password='123')
-    print(f'{user = }')
-    print(f'{user.username = }')
+    user = User.create_and_commit(username='ali', password='123')
+    # print(f'{user = }')
+    # print(f'{user.username = }')
+    get_user = User.list()
     # get_user = User.get_one(username='ali', password='123')
-    # print(f'{get_user = }')
+    print(f'{get_user = }')
+    for u in get_user:
+        print(f'{u.id = }')
+        print(f'{u.username = }')
+
+    last_user = User.last()
     # print(f'{dir(request) = }')
     # print(f'{request.query_params = }')
     # raise UserNotFound
-    return Response(status_code=200, data=request.data)
+    a = [
+        {
+            'id': 1,
+            'username': 'ali',
+            'password': '111',
+        },
+        {
+            'id': 2,
+            'username': 'ali2',
+            'password': '1112',
+        },
+        {
+            'id': 3,
+            'username': 'ali3',
+            'password': '1113',
+        }
+    ]
+    b = 'asdf'
+    c = {1, 2, 4}
+
+    class A:
+        ...
+    d = A()
+    e = True
+    return Response(status_code=200, data=e)
+
 
 
 # @API.post(input=UserInputSerializer, output_model=UserSerializer)
--- panther/app.py
+++ panther/app.py
@@ -1,3 +1,4 @@
+from panther.db import BaseModel
 from pydantic import ValidationError
 from panther.request import Request
 from panther.exceptions import APIException
@@ -17,15 +18,22 @@ class API:
                 raise APIException(status_code=400, detail=error)
 
     @classmethod
-    def get(cls, output_model):
-        def decorator(func):
-            async def wrapper(*args, **kwargs):
-                response = await func(*args, **kwargs)
-                _data = output_model(**response._data)
-                response.set_data(_data.dict())
-                return response
-            return wrapper
-        return decorator
+    def clean_output(cls, data, output_model):
+        if output_model is None:
+            return data
+
+        if issubclass(type(data), BaseModel):
+            _data = output_model(**data.dict()).dict()
+        elif isinstance(data, dict):
+            _data = output_model(**data).dict()
+        elif isinstance(data, list) or isinstance(data, tuple):
+            _data = [output_model(**d).dict() for d in data]
+        elif isinstance(data, str) or isinstance(data, bool) or isinstance(data, set):
+            raise TypeError('Type of Response data is not match with output_model. '
+                            '\n*hint: You may want to pass None to output_model')
+        else:
+            raise TypeError(f"Type of Response 'data' is not valid.")
+        return _data
 
     @classmethod
     def post(cls, input_model=None, output_model=None):
@@ -34,8 +42,8 @@ class API:
                 request: Request = kwargs['request']
                 cls.validate_input(data=request.data, input_model=input_model)
                 response = await func(request=request)
-                _data = output_model(**response._data)
-                response.set_data(_data.dict())
+                data = cls.clean_output(data=response._data, output_model=output_model)
+                response.set_data(data)
                 return response
             return wrapper
         return decorator
@@ -47,8 +55,19 @@ class API:
                 request: Request = kwargs['request']
                 cls.validate_input(data=request.data, input_model=input_model)
                 response = await func(request=request)
-                _data = output_model(**response._data)
-                response.set_data(_data.dict())
+                data = cls.clean_output(data=response._data, output_model=output_model)
+                response.set_data(data)
+                return response
+            return wrapper
+        return decorator
+
+    @classmethod
+    def get(cls, output_model):
+        def decorator(func):
+            async def wrapper(*args, **kwargs):
+                response = await func(*args, **kwargs)
+                data = cls.clean_output(data=response._data, output_model=output_model)
+                response.set_data(data)
                 return response
             return wrapper
         return decorator
@@ -58,10 +77,8 @@ class API:
         def decorator(func):
             async def wrapper(*args, **kwargs):
                 response = await func(*args, **kwargs)
-                _data = output_model(**response._data)
-                response.set_data(_data.dict())
+                data = cls.clean_output(data=response._data, output_model=output_model)
+                response.set_data(data)
                 return response
             return wrapper
         return decorator
-
-
