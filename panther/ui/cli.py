becc8790c79aa0b2e2d3a87b774acc2e7aa531af
MahanBi <DirtyMBGamer@gmail.com>
2022-04-03 10:32:03 -0700

    Del: run cmd

--- panther/ui/cli.py
+++ panther/ui/cli.py
@@ -14,8 +14,6 @@ def parser(args):
     match args:
         case {'version': True}:
             print('0.1.4')
-        case {'host': _, 'port': _}:
-            uvicorn.run('main:app', host=args.get('host'), port=args.get('port'))
         case {'name': _, 'path': _}:
             if OS_name == 'nt':
                 subprocess.call([ui_folder / 'project.bat', args.get('path'), args.get('name')])
@@ -32,10 +30,6 @@ def main():
     arg_parser = ArgumentParser(description='Panther, Fast & Easy Python Framework.')
     arg_parser.add_argument('-v', '--version', action='store_true', help='panther version.')
     sub_parser = arg_parser.add_subparsers(title='Main Commands')
-    # run command
-    run_parser = sub_parser.add_parser(name='run', help='run server.')
-    run_parser.add_argument('-host', default='127.0.0.1', type=str, help='host')
-    run_parser.add_argument('-port', default=8000, type=int, help='port')
     # make project command
     make_project_parser = sub_parser.add_parser(name='makeproject', help='make project files.')
     make_project_parser.add_argument('name', type=str, help='project name.')
