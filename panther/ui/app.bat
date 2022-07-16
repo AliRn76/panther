0b56ac8cfce75a715b3cdc0f815e067e6b2434c9
MahanBi <dirtymbgamer@gmail.com>
2022-07-17 03:11:38 +0430

    Change: Arg to TypedDict

--- panther/ui/app.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-Rem: Function for create apps
-SET path=%~1\%~2
-SET app_folder=%~dp0template\app\
-ECHO D | C:\Windows\system32\xcopy /s %app_folder% %path%
--- panther/ui/project.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-Rem: Function for create project files
-SET path=%~1\%~2
-SET pro_folder=%~dp0template\project\
-ECHO D | C:\Windows\system32\xcopy /s %pro_folder% %path%
-
