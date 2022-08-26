fc2379ce8467dd543a084549220201fde7fd6155
AliRn76 <alirn76@yahoo.com>
2022-08-26 16:41:02 +0430

    Add perf_counter to query_logger

--- panther/db/utils.py
+++ panther/db/utils.py
@@ -1,11 +1,18 @@
 from panther.logger import logger
+from time import perf_counter
 
 
 def query_logger(func):
     def log(*args, **kwargs):
-        # TODO: Time Log
-        # logger.info(f'\033[1mQuery -->\033[0m {func.__name__}')
-        logger.info(f'Query --> {func.__name__}')
-        return func(*args, **kwargs)
+        # TODO: Log Only If Debug Is True
+        Debug = False
+        if Debug is False:
+            return func(*args, **kwargs)
+        start = perf_counter()
+        response = func(*args, **kwargs)
+        end = perf_counter()
+        # logger.info(f'\033[1mQuery -->\033[0m  {args[0].__name__}.{func.__name__}() --> {(end - start) * 1_000:.2} ms')
+        logger.info(f'Query --> {args[0].__name__}.{func.__name__}() --> {(end - start) * 1_000:.2} ms')
+        return response
     return log
 
