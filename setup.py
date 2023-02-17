96c46433b268e6c3bc195f65600c129a4b934d5d
AliRn <alirn1997@gmail.com>
2023-02-18 02:09:16 +0330

    Add EXTRAS_REQUIRE +

--- setup.py
+++ setup.py
@@ -9,26 +9,34 @@ def panther_version() -> str:
         return re.search("__version__ = ['\"]([^'\"]+)['\"]", f.read()).group(1)
 
 
-_version = panther_version()
-_long_description = open('README.md').read()
+VERSION = panther_version()
+DESCRIPTION = open('README.md').read()
+
+EXTRAS_REQUIRE = {
+    'full': [
+        'redis>=4.5.1',
+        'pymongo>=4.3.3',
+        'python-jose>=3.3.0',
+        'bpython>=0.24',
+    ]
+}
 
 setup(
     name='panther',
-    version=_version,
-    python_requires='>=3.10',
+    version=VERSION,
+    python_requires='>=3.11',
     author='Ali RajabNezhad',
     author_email='alirn76@yahoo.com',
     keywords='fast friendly web framework',
     url='https://github.com/alirn76/panther',
-    description='Fast, Friendly Python Web Framework',
-    long_description=_long_description,
+    description='Fast & Friendly Python Web Framework',
+    long_description=DESCRIPTION,
     long_description_content_type='text/markdown',
     include_package_data=True,
     license='MIT',
     classifiers=[
-        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.10',
+        'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.11',
     ],
     entry_points={
@@ -39,12 +47,10 @@ setup(
     },
     install_requires=[
         'uvicorn[standard]',
-        'orjson>=3.7.12',
-        'pydantic>=1.9.2',
-        'redis>=4.3.4',
-        'pymongo>=4.2.0',
-        'bpython>=0.23',
-        'tinydb>=4.7.0',
-        'rich>=12.5.1',
+        'pydantic>=1.10.5',
+        'tinydb>=4.7.1',
+        'orjson>=3.8.6',
+        'rich>=13.3.1',
     ],
+    extras_require=EXTRAS_REQUIRE,
 )
