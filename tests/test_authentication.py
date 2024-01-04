317c869c13f09bcf64542a1bf20410884dea1a0e
Ali RajabNezhad <alirn76@yahoo.com>
2024-01-04 23:27:22 +0330

    Clean old tests

--- tests/test_authentication.py
+++ tests/test_authentication.py
@@ -10,6 +10,7 @@ from panther.db.queries import Query
 from panther.db.queries.pantherdb_queries import BasePantherDBQuery
 from panther.request import Request
 from panther.test import APIClient
+from tests._utils import check_two_dicts
 
 
 @API()
@@ -178,9 +179,11 @@ class TestAuthentication(TestCase):
         with self.assertNoLogs(level='ERROR'):
             res = self.client.get('auth-required', headers=self.TOKEN)
 
+        expected_response = {
+            'id': 1,
+            'username': 'Username',
+            'password': 'Password'
+        }
         assert res.status_code == 200
-        assert [*res.data.keys()] == ['id', 'username', 'password']
-        assert res.data['id'] == 1
-        assert res.data['username'] == 'Username'
-        assert res.data['password'] == 'Password'
+        assert check_two_dicts(res.data, expected_response)
 
