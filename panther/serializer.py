6f0a24b9276b3a246ed51a5bc7134393e27139f9
Ali RajabNezhad <alirn76@yahoo.com>
2024-01-23 01:14:12 +0330

    Add ModelSerializer example

--- panther/serializer.py
+++ panther/serializer.py
@@ -5,12 +5,20 @@ from pydantic_core._pydantic_core import PydanticUndefined
 class ModelSerializer:
     def __new__(cls, *args, **kwargs):
         model_fields = kwargs['model'].model_fields
+        model_name = args[0]
         field_definitions = {}
         for field_name in args[2]['fields']:
+            if field_name not in model_fields:
+                msg = f"{model_name}('{field_name}' is not in '{kwargs['model'].__name__}')"
+                raise KeyError(msg) from None
+
             field_definitions[field_name] = (model_fields[field_name].annotation, model_fields[field_name])
         for required in args[2]['required_fields']:
+            if required not in field_definitions:
+                msg = f"{model_name}('{required}' is in 'required_fields' but not in 'fields')"
+                raise KeyError(msg) from None
             field_definitions[required][1].default = PydanticUndefined
         return create_model(
-            __model_name=args[0],
+            __model_name=model_name,
             **field_definitions
         )
