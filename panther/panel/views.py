0cdac87e59a096cc0ac680c65d78c20f4166c19d
Ali RajabNezhad <alirn76@yahoo.com>
2024-11-22 00:14:10 +0330

    Support `application/x-www-form-urlencoded` Content Type

--- panther/panel/views.py
+++ panther/panel/views.py
@@ -1,12 +1,12 @@
 from panther import status
-
 from panther.app import API, GenericAPI
 from panther.configs import config
-from panther.exceptions import RedirectAPIError
+from panther.db.models import BaseUser
+from panther.exceptions import RedirectAPIError, AuthenticationAPIError
 from panther.panel.utils import get_model_fields, get_models
 from panther.permissions import BasePermission
 from panther.request import Request
-from panther.response import TemplateResponse, Response, Cookie
+from panther.response import TemplateResponse, Response, Cookie, RedirectResponse
 
 
 class AdminPanelPermission(BasePermission):
@@ -14,17 +14,32 @@ class AdminPanelPermission(BasePermission):
     async def authorization(cls, request: Request) -> bool:
         if 'cookie' in request.headers:
             return True
-        raise RedirectAPIError(url='login')
+        raise RedirectAPIError(url=f'login?redirect_to={request.path}')
 
 
 class LoginView(GenericAPI):
     def get(self):
-        return TemplateResponse(path='login.html', context={'tables': get_models()})
+        return TemplateResponse(path='login.html')
 
-    def post(self, request: Request):
-        return Response(
-            status_code=status.HTTP_200_OK,
-            set_cookies=[Cookie(key='token', value='this is token', max_age=120)]
+    async def post(self, request: Request):
+        # # COMMENTED FOR DEVELOPMENT
+        # user: BaseUser = await config.USER_MODEL.find_one({config.USER_MODEL.USERNAME_FIELD: request.data['username']})
+        # if user is None:
+        #     raise AuthenticationAPIError
+        #
+        # if user.check_password(new_password=request.data['password']) is False:
+        #     return AuthenticationAPIError
+
+        return RedirectResponse(
+            url=request.query_params.get('redirect_to', '..'),
+            status_code=status.HTTP_302_FOUND,
+            set_cookies=[
+                Cookie(
+                    key='access_token',
+                    value=config.AUTHENTICATION.encode_jwt(str(self.id)),
+                    max_age=config.JWT_CONFIG.life_time
+                )
+            ]
         )
 
 
