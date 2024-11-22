b69c85c9e7df7254ceaaa96ae9890df97bdc2231
Ali RajabNezhad <alirn76@yahoo.com>
2024-11-22 18:56:10 +0330

    Add `clean_model_schema()`

--- panther/panel/utils.py
+++ panther/panel/utils.py
@@ -1,7 +1,43 @@
+from collections import defaultdict
+
 from panther.configs import config
 from panther.db.models import Model
 
 
+def _ref_name(ref: str) -> str:
+    obj_name = ref.rsplit('/', maxsplit=1)[1]
+    return f'${obj_name}'
+
+
+def clean_model_schema(schema: dict) -> dict:
+    result = defaultdict(dict)
+    result['title'] = schema['title']
+    if '$defs' in schema:
+        for sk, sv in schema['$defs'].items():
+            result['$'][sk] = clean_model_schema(sv)
+
+    for k, v in schema['properties'].items():
+        result['fields'][k] = {}
+        if 'title' in v:
+            result['fields'][k]['title'] = v['title']
+        if 'type' in v:
+            result['fields'][k]['type'] = [v['type']]
+        elif 'anyOf' in v:
+            result['fields'][k]['type'] = [i['type'] if 'type' in i else _ref_name(i['$ref']) for i in v['anyOf']]
+        if 'default' in v:
+            result['fields'][k]['default'] = v['default']
+
+        if '$ref' in v:  # For obj
+            result['fields'][k]['type'] = _ref_name(v['$ref'])
+
+        if 'items' in v:  # For array
+            result['fields'][k]['items'] = _ref_name(v['items']['$ref'])
+
+        result['fields'][k]['required'] = k in schema['required']
+    return result
+
+
+# TODO: Remove this
 def get_model_fields(model):
     result = {}
 
@@ -16,6 +52,7 @@ def get_model_fields(model):
         else:
             result[k] = getattr(v.annotation, '__name__', str(v.annotation))
     return result
+
 
 def get_models():
     return [{
